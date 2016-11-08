c1777c2f8078b618a329e6312609cd0dd1628911
John Gasper <John@john-gaspers-macbook-pro.local>
2016-11-07 21:12:30 -0600

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by John Gasper on 11/7/16.
+//  Copyright © 2016 Carissa Gasper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
